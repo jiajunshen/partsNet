7fac5081bcee4f4e5c44466adc76f80dc54a4eea
Jiajun <shenjiajun90@gmail.com>
2016-04-21 16:49:00 -0500

    check loglikelihood calculation

--- pnet/mixture_classification_layer.py
+++ pnet/mixture_classification_layer.py
@@ -3,6 +3,7 @@ from pnet.layer import Layer
 import numpy as np
 import amitgroup as ag
 from pnet.bernoullimm import BernoulliMM
+from sklearn.utils.extmath import logsumexp
 
 @Layer.register('mixture-classification-layer')
 class MixtureClassificationLayer(SupervisedLayer):
@@ -11,6 +12,7 @@ class MixtureClassificationLayer(SupervisedLayer):
         self._min_prob = min_prob
         self._models = None
         self._block_size = block_size
+        self._modelinstance = None
     @property
     def trained(self):
         return self._models is not None
@@ -18,6 +20,20 @@ class MixtureClassificationLayer(SupervisedLayer):
     @property
     def classifier(self):
         return True
+
+    def check_correctness (self, X_all):
+        for i in range(11):
+            theta = self._models[i]
+            Yhat = np.zeros(X_all.shape[0])
+            Yhat_score = np.zeros(X_all.shape[0])
+            
+            X = X_all[0]
+            XX =  X[np.newaxis]
+            llh = np.sum(XX * np.log(theta) + (1 - XX) * np.log(1 - theta), axis = (-3, -2, -1)) + np.log(self._modelinstance[i].weights_)
+            lpr = logsumexp(llh, axis = 0)
+            print(np.sum(lpr))
+            print(self._modelinstance[i].score(X.reshape(1, 512)))
+        
 
     def extract(self, X_all):
         #print "mixture classification extract started"
@@ -31,7 +47,6 @@ class MixtureClassificationLayer(SupervisedLayer):
                 X = X_all[i:blockend]
                 XX =  X[:,np.newaxis,np.newaxis]
                 llh = XX * np.log(theta) + (1 - XX) * np.log(1 - theta)
-                print(llh.shape)
                 Yhat[i:blockend] = np.argmax(np.apply_over_axes(np.sum, llh, [-3, -2, -1])[...,0,0,0].max(-1), axis=1)
         #print "mixture classification extract finished"
         #print(Yhat.shape)
@@ -40,6 +55,7 @@ class MixtureClassificationLayer(SupervisedLayer):
     def train(self, X, Y, OriginalX = None):
         K = Y.max() + 1
         mm_models = []
+        self._modelinstance = []
         for k in range(K):
             Xk = X[Y == k]
             #import pdb ; pdb.set_trace()
@@ -50,6 +66,7 @@ class MixtureClassificationLayer(SupervisedLayer):
             mm = BernoulliMM(n_components=self._n_components, n_iter=10, n_init=1, random_state=0, min_prob=self._min_prob,blocksize = self._block_size)
             mm.fit(Xk)
             mm_models.append(mm.means_.reshape((self._n_components,)+X.shape[1:]))
+            self._modelinstance.append(mm)
 
         self._models = np.asarray(mm_models)
         #print "mixtureclassification-layer training finished"
